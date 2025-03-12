c58497c37760c0449deb833127e96f65ab40bd9f
cristhianDavidOrbes <134344774+cristhianDavidOrbes@users.noreply.github.com>
2025-03-12 08:11:01 -0500

    portafiolio

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center  w-screen h-screen bg-cyan-300 ">
+      <h1 className="text-gray-500 font-extrabold font-mono text-4xl">Cristhian david orbes</h1>
+    </main>
+  );
+}
