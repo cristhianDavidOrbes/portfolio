e819473eeb4b34f4b017240ef6a47d9c98d41b22
cristhianDavidOrbes <134344774+cristhianDavidOrbes@users.noreply.github.com>
2025-04-29 20:06:22 -0500

    font and colors
    primer commit, integracion de colores y fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center  w-screen h-screen bg-cyan-300 ">
-      <h1 className="text-gray-500 font-extrabold font-mono text-4xl">Cristhian david orbes</h1>
+    <main className="flex items-center justify-center bg-azul-fondos  w-screen h-screen  ">
+      <h1 className="text-azul-mas-claro font-extrabold font-primary text-4xl">Cristhian david orbes</h1>
     </main>
   );
 }
